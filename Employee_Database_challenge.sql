16cee245ebec8880f2b624b21f03b0be20374e47
Sean Gaynor <107370477+Knicks2020@users.noreply.github.com>
2022-08-14 19:31:12 -0400

    Updating 8/14/22

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,21 @@
+-- A query is written and executed to create a Retirement Titles table 
+-- or employees who are born between January 1, 1952 and December 31, 1955
+SELECT e.emp_no, e.first_name, e.last_name,
+		t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees e
+JOIN titles t on (t.emp_no = e.emp_no)
+WHERE e.birth_date between '1952-01-01' and '1955-01-01'
+	order by e.emp_no
+
+
+
+
+
+
+
+
+
 SELECT COUNT (rt.emp_no),
 		rt.title
 INTO retiring_titles
